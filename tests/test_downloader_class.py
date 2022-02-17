b8241edc89ec2074c53f985a5391babf10c363e0
jason.beach <jason.beach@jasonbeachs-MBP.fios-router.home>
2022-02-17 12:51:51 -0500

    add get_urls() test

--- tests/test_downloader_class.py
+++ tests/test_downloader_class.py
@@ -5,11 +5,12 @@ relative and absolute download folder paths.
 
 import os
 from pathlib import Path
+from datetime import date
 
 import pytest
 
 from sec_edgar_downloader import Downloader
-from sec_edgar_downloader._constants import SUPPORTED_FILINGS
+from sec_edgar_downloader._constants import SUPPORTED_FILINGS, DATE_FORMAT_TOKENS
 
 
 def test_constructor_no_params():
@@ -50,3 +51,57 @@ def test_supported_filings(downloader):
     dl, _ = downloader
     expected = sorted(SUPPORTED_FILINGS)
     assert dl.supported_filings == expected
+
+
+def test__check_params():
+    # SEC Edgar Search fails to retrieve Apple 8-Ks after 2000 and before 2002
+    formatted_earliest_after_date = date(2002, 1, 1).strftime(DATE_FORMAT_TOKENS)
+
+    filing_type = "8-K"
+    ticker = "AAPL"
+    before_date = date(2019, 11, 15).strftime(DATE_FORMAT_TOKENS)
+    include_amends = False
+    # num_filings_to_download < number of filings available
+    num_filings_to_download = 100
+
+    dl = Downloader("./Downloads")
+    filing, ticker_or_cik, amount, after, before, included_amends, query = dl._check_params(
+        filing_type,
+        ticker,
+        num_filings_to_download,
+        formatted_earliest_after_date,
+        before_date,
+        include_amends,
+    )
+
+    assert (filing == filing_type) and (
+            ticker_or_cik == ticker) and (
+            amount == num_filings_to_download) and (
+            after == formatted_earliest_after_date) and (
+            before == before_date) and (
+            included_amends == include_amends) and (
+            query == "" )
+
+
+def test_get_urls():
+    # SEC Edgar Search fails to retrieve Apple 8-Ks after 2000 and before 2002
+    formatted_earliest_after_date = date(2002, 1, 1).strftime(DATE_FORMAT_TOKENS)
+
+    filing_type = "8-K"
+    ticker = "AAPL"
+    before_date = date(2019, 11, 15).strftime(DATE_FORMAT_TOKENS)
+    include_amends = False
+    # num_filings_to_download < number of filings available
+    num_filings_to_download = 100
+
+    dl = Downloader("./Downloads")
+    urls = dl.get_urls(
+        filing=filing_type,
+        ticker_or_cik = ticker,
+        amount = num_filings_to_download,
+        after = formatted_earliest_after_date,
+        before = before_date,
+        include_amends=include_amends,
+    )
+
+    assert len(urls) == num_filings_to_download
